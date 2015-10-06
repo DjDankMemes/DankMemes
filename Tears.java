e372fb5c8948a2ce48ebd2620d1e54290300b22a
DjDankMemes <jsailors17@lvcp.org>
2015-10-06 14:39:19 -0700

    Create Tears.java

---
+++ Tears.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Tears{
+        public static void main(String[]args){
+          Cri af=new Cri();
+        Scanner sc=new Scanner(System.in);
+            af.setX(sc.nextDouble());
+            af.graf();
+        System.out.println(af.getY());
+        }
+}
